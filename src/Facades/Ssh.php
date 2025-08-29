9cd1078678a7cd1cd91ef8ec4b7e87b93323af32
Leandro H Agostinho <leandroh.ago@gmail.com>
2025-08-29 11:10:02 -0300

    Add docblock

--- src/Facades/Ssh.php
+++ src/Facades/Ssh.php
@@ -5,6 +5,8 @@ namespace Nave\IssSatellite\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Nave\IssSatellite\Ssh connection(string $connectionName)
+ * @method static \Nave\IssSatellite\Ssh connect(bool $debug = false)
  * @see \Nave\IssSatellite\Ssh
  */
 class Ssh extends Facade
